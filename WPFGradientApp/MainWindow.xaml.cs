b87e3ce47955d42222cbafffb093b29c7f40cf78
jonteohr <jonathan.ohrstrom@saabgroup.com>
2024-04-12 13:19:19 +0200

    Added dynamic welcome titlte

--- WPFGradientApp/MainWindow.xaml.cs
+++ WPFGradientApp/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System.Runtime.InteropServices;
+using System.Timers;
 using System.Windows;
 using System.Windows.Input;
+using Timer = System.Timers.Timer;
 
 namespace WPFGradientApp;
 
@@ -12,18 +14,72 @@ public partial class MainWindow : Window
     
     public const int WM_NCLBUTTONDOWN = 0xA1;
     public const int HT_CAPTION = 0x2;
-
-    [System.Runtime.InteropServices.DllImport("user32.dll")]
+    [DllImport("user32.dll")]
     public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
-    [System.Runtime.InteropServices.DllImport("user32.dll")]
+    [DllImport("user32.dll")]
     public static extern bool ReleaseCapture();
     [DllImport("user32.dll")]
     private static extern IntPtr GetForegroundWindow();
 
+    private int m_typeSpeed = 150;
+    private int m_timerspeed = 3;
+    private bool m_typing;
     
+    public string WindowTitle { get; private set; }
+    private string[] Titles = 
+        [
+            "A nice app",
+            "App title",
+            "Cool title",
+            "Useful stuff!",
+            "This title changes",
+            "Captivating title",
+            "Lorem ipsum"
+        ];
+
     public MainWindow()
     {
+        WindowTitle = GetRandomTitle();
+        
         InitializeComponent();
+        DataContext = this;
+        
+        var timer = new Timer();
+        timer.Interval = m_timerspeed * 1000;
+        timer.AutoReset = true;
+        timer.Elapsed += TimerOnElapsed;
+        timer.Enabled = true;
+        timer.Start();
+    }
+
+    private void TimerOnElapsed(object? sender, ElapsedEventArgs e)
+    {
+        while (!m_typing)
+        {
+            m_typing = true;
+            for (var i = WindowTitle.Length - 1; i < WindowTitle.Length; i--)
+            {
+                if (i < 0)
+                    break;
+            
+                WindowTitle = WindowTitle.Remove(i);
+                SafeThreadInvoker(() => TitleLabel.Content = WindowTitle);
+                Thread.Sleep(m_typeSpeed);
+            }
+
+            WindowTitle = GetRandomTitle();
+
+            for (var i = 0; i <= WindowTitle.Length; i++)
+            {
+                var tmp = WindowTitle.Substring(0, i);
+                SafeThreadInvoker(() => TitleLabel.Content = tmp);
+                Thread.Sleep(m_typeSpeed);
+            }
+
+            break;
+        }
+        
+        m_typing = false;
     }
 
     private void CloseBtn_OnClick(object sender, RoutedEventArgs e)
@@ -43,5 +99,24 @@ public partial class MainWindow : Window
     private void MinimizeBtn_OnClick(object sender, RoutedEventArgs e)
     {
         WindowState = WindowState.Minimized;
+    }
+
+    private void SafeThreadInvoker(Action a)
+    {
+        if (!CheckAccess())
+            Dispatcher.Invoke(a);
+        else
+            a();
+    }
+
+    private string GetRandomTitle()
+    {
+        var rand = new Random();
+
+        var generated = Titles[rand.Next(0, Titles.Length)];
+        while (generated == WindowTitle)
+            generated = Titles[rand.Next(0, Titles.Length)];
+
+        return generated;
     }
 }
